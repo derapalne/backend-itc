4bc92c305b07a00263837bfc7bee136fa5ca87a6
dera <derapalne@gmail.com>
2024-01-04 16:26:36 -0300

    Module Updating

--- src/modules/user.module.ts
+++ src/modules/user.module.ts
@@ -5,10 +5,13 @@ import { User } from 'src/models/user.model';
 import { UserController } from 'src/controllers/user.controller';
 import { UserSearchService } from 'src/services/userSearch.service';
 import { UserSearch } from 'src/models/userSearch.model';
+import { Cart } from 'src/models/cart.model';
+import { CartService } from 'src/services/cart.service';
+import { CartProduct } from 'src/models/cartProduct.model';
 
 @Module({
-  imports: [SequelizeModule.forFeature([User, UserSearch])],
-  providers: [UserService, UserSearchService],
+  imports: [SequelizeModule.forFeature([User, UserSearch, Cart, CartProduct])],
+  providers: [UserService, UserSearchService, CartService],
   exports: [UserService],
   controllers: [UserController],
 })

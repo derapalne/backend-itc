c8a32367f42931db776d5e78b01aaf26ad22b0b5
dera <derapalne@gmail.com>
2024-01-15 14:22:53 -0300

    Cart Controller

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -7,6 +7,7 @@ import {
   UseGuards,
 } from '@nestjs/common';
 import { JwtAuthGuard } from 'src/guards/jwt-auth.guard';
+import { CartService } from 'src/services/cart.service';
 import { UserService } from 'src/services/user.service';
 import { UserSearchService } from 'src/services/userSearch.service';
 
@@ -15,6 +16,7 @@ export class UserController {
   constructor(
     private userService: UserService,
     private userSearchService: UserSearchService,
+    private cartService: CartService,
   ) {}
   @Get('/')
   async getAllUsers() {
@@ -43,10 +45,40 @@ export class UserController {
   @UseGuards(JwtAuthGuard)
   @Get('/last-search')
   async getActiveUsersLastSearch(@Request() req) {
+    // Fix "any" typing
     try {
-      // Fix "any" typing
       return await this.userSearchService.getUserLastSearch(req.user.userId);
     } catch (error) {
+      if (error.message) throw new HttpException(error.message, 500);
+      throw new HttpException(error, 500);
+    }
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('/active-cart')
+  async getActiveCart(@Request() req) {
+    // Fix "any" typing
+    try {
+      const cart = await this.cartService.getActiveCartByUserId(
+        req.user.userId,
+      );
+      console.log('cart', cart);
+      return cart ?? { id: 0, products: [] };
+    } catch (error) {
+      console.log(error);
+      if (error.message) throw new HttpException(error.message, 500);
+      throw new HttpException(error, 500);
+    }
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('/ordered-carts')
+  async getOrderedCarts(@Request() req) {
+    // Fix "any" typing
+    try {
+      return await this.cartService.getOrderedCartsByUserId(req.user.userId);
+    } catch (error) {
+      console.log(error);
       if (error.message) throw new HttpException(error.message, 500);
       throw new HttpException(error, 500);
     }

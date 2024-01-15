18cbe3f9c139ded94472df0241043ed4691e7b97
dera <derapalne@gmail.com>
2024-01-15 13:33:11 -0300

    Longer session fix

--- src/modules/auth.module.ts
+++ src/modules/auth.module.ts
@@ -15,7 +15,7 @@ import { JwtStrategy } from 'src/auth/jwt.strategy';
     JwtModule.register({
       secret: process.env['JWT_SECRET'],
       secretOrPrivateKey: process.env['JWT_SECRET'],
-      signOptions: { expiresIn: '600s' },
+      signOptions: { expiresIn: '6000s' },
     }),
   ],
 })
